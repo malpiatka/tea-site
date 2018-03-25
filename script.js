97d940b3b2ea1da34ac49e2d1755c7b1b893f169
Magdalena Mielke <magdalena.mielke90@gmail.com>
2018-03-25 17:09:19 +0200

    navbar, img

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+(function() {
 
+    let menu = {
+        navToggle: document.querySelector('.nav-toggle'),
+        nav: document.querySelector('nav'),
+
+        doToggle: function(event) {
+            event.preventDefault();
+            this.navToggle.classList.toggle('expanded');
+            this.nav.classList.toggle('expanded');
+        }
+    };
+
+    menu.navToggle.addEventListener('click', function(event) {
+        menu.doToggle(event);
+    });
+}());
