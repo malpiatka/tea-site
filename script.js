08a87e685914bf5c3f2d678423f12d05053d3565
Magdalena Mielke <magdalena.mielke90@gmail.com>
2018-03-26 19:40:42 +0200

    accordion

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
 //menu expanded
-(function() {
+(function () {
 
     let menu = {
         navToggle: document.querySelector('.nav-toggle'),
         nav: document.querySelector('nav'),
 
-        doToggle: function(event) {
+        doToggle: function (event) {
             event.preventDefault();
             this.navToggle.classList.toggle('expanded');
             this.nav.classList.toggle('expanded');
         }
     };
 
-    menu.navToggle.addEventListener('click', function(event) {
+    menu.navToggle.addEventListener('click', function (event) {
         menu.doToggle(event);
     });
 }());
 
+//accordion
+let accordions = document.getElementsByClassName('accordion-title');
+
+for (let i = 0; i <= accordions.length; i++) {
+    accordions[i].onclick = function () {
+        let content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    }
+}
